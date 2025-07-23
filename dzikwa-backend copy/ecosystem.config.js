34e0eecfdb3dc64cbb2ad93bde7077bc1ce58f7c
Talent A Chiradza <talentachiradza@gmail.com>
2025-07-23 16:43:50 +0200

    new changes

---
+++ dzikwa-backend copy/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'server',
+    script: 'server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 5000
+    },
+    env_development: {
+      NODE_ENV: 'development',
+      PORT: 5000
+    }
+  }]
+};
